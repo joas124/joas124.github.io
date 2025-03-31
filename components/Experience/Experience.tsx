6f89e95cede6d83dd6a06094970ed24b4ec1d872
joas124 <joasdavi16@gmail.com>
2025-03-31 12:26:58 +0100

    Optimize images

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -6,6 +6,8 @@ type experienceType = {
   img: string,
   imgAlt: string,
   imgClass: string,
+  imgWidth: number,
+  imgHeight: number,
   title: string,
   description: string | ReactNode
 }
@@ -13,7 +15,7 @@ type experienceType = {
 export default function Experience({experience}: {experience: experienceType}){  
   return(
     <div className={styles.history}>
-      <Image src={experience.img} className={styles[experience.imgClass]} alt={experience.imgAlt} />
+      <Image src={experience.img} className={styles[experience.imgClass]} width={experience.imgWidth} height={experience.imgHeight} alt={experience.imgAlt} />
       <div className={styles.study}>
         <h4>{experience.title}</h4>
         <p>{experience.description}</p>
--- components/Header/HeaderActions.tsx
+++ components/Header/HeaderActions.tsx
@@ -29,14 +29,14 @@ export default function HeaderActions() {
     <div className={styles.buttons}>
       <button className={styles.print} onClick={() => window.print()}>
         {t('print')}
-        <Image src='./icons/printer.svg' alt='Printer Icon' />
+        <Image src='./icons/printer.svg' width={30} height={30} alt='Printer Icon' />
       </button>
       <select className={styles.langSelect} name='language' value={locale} onChange={handleLanguageChange}>
         <option value='en'>English</option>
         <option value='pt'>Português</option>
       </select>
       <button className={styles.dmButton} onClick={toggleDarkMode}>
-        <Image src={darkMode ? './icons/moon-stars-fill.svg' : './icons/brightness-high-fill.svg'} alt='Dark Mode Button' />
+        <Image src={darkMode ? './icons/moon-stars-fill.svg' : './icons/brightness-high-fill.svg'} width={16} height={16} alt='Dark Mode Button' />
       </button>
     </div>
   );
